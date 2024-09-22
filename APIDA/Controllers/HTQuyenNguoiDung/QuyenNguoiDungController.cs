f3754a0e28cd39fcdf835a9b5a3f5d65690bdc47
tienanh0903k <tienanh2003k@gmail.com>
2024-09-22 11:12:13 +0700

    upload PhanquyenchoUser

---
+++ APIDA/Controllers/HTQuyenNguoiDung/QuyenNguoiDungController.cs
@@ -0,0 +1,43 @@
+﻿using APIPCHY.Models.DMTruongYCTN;
+using APIPCHY.Models.HTQuyenNguoiDung;
+using APIPCHY.Models.NguoiKy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIPCHY.Controllers
+{
+    [Route("APIPCHY/[controller]")]
+    [ApiController]
+    public class QuyenNguoiDungController : ControllerBase
+    {
+        QuyenNguoiDungManager db = new QuyenNguoiDungManager();
+
+
+        [HttpGet("get")]
+        public IActionResult getData_QuyenNguoiDung()
+        {
+            return Ok(db.Get_QUYEN_NGUOIDUNG());
+        }
+
+        [HttpPost("create")]
+        public void PostInsertQuyen([FromBody] HTQuyenNguoiDung quyen)
+        {
+            db.Insert_HT_QUYEN_NGUOIDUNG(quyen);
+        }
+
+        [HttpPatch("update")]
+        public void UpdateQuyen([FromBody] HTQuyenNguoiDung quyen)
+        {
+            db.Update_HT_QUYEN_NGUOIDUNG(quyen);
+        }
+
+        [HttpGet("delete/{id}")]
+        //[Authorize]
+        public IActionResult DeleteQuyen(int id)
+        {
+            db.Delete_HT_QUYEN_NGUOIDUNG(id);
+            return Ok("Xoa thanh cong");
+        }
+
+    }
+}
---
+++ APIDA/Models/HTQuyenNguoiDung/HTQuyenNguoiDung.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIPCHY.Models.HTQuyenNguoiDung
+{
+    public class HTQuyenNguoiDung
+    {
+        public int ID { get; set; }
+        public string MA_NGUOI_DUNG { get; set; }
+        public string MA_NHOM_TV { get; set; }
+
+        public string? TenNguoiDung { get; set; }
+        public string? TenDonVi { get; set; }
+        public string? TenNhom { get; set; }
+
+
+        public DateTime? NGAY_TAO
+        {
+            get; set;
+        }
+        
+
+    }
+}
